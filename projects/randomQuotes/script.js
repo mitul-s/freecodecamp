0f1a5d5be61ded090605c935fc18b627011d4a9c
Mitul Shah <mitulxshah@gmail.com>
2017-02-26 23:58:40 -0500

    api set up well

--- projects/randomQuotes/script.js
+++ projects/randomQuotes/script.js
@@ -1,14 +1,49 @@
+/*
 var authorName 	= document.querySelector('.author');
 var newQuote 	= document.querySelector('.quote');
 
 var xhr = new XMLHttpRequest();
-/*xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&appid=4ddb1b82dfbc99cb9eedfc8c2f096deb&units=metric", false);*/
 xhr.open("GET", "http://quotes.stormconsultancy.co.uk/random.json", false);
 xhr.send();
 
 var quoteData = JSON.parse(xhr.response);
 
+var request = new XMLHttpRequest();
+var url = "http://quotes.stormconsultancy.co.uk/random.json";
+request.open("GET", url, true);
+
 function insert(){
-	authorName.innerHTML = quoteData.author;
-	newQuote.innerHTML = quoteData.quote;
+	location.reload();
 }
+
+request.onload = function(){
+	var data = JSON.parse(request.response);
+	authorName.innerHTML = data.author;
+	newQuote.innerHTML = data.quote;	
+}
+
+request.send();
+*/
+
+$(document).ready(getQuote);
+
+function getQuote() {
+  $.ajax({
+    dataType: "jsonp",
+    jsonpCallback: "parseQuote",
+    url: "http://quotes.stormconsultancy.co.uk/random.json",
+    success: function(results) {
+      $('.quote').text(results.quote);
+      $('.author').text(results.author);
+    }
+  });
+}
+
+$(document).ready(function() {
+  // get a random quote when page loads
+  getQuote();
+  $('.btn').click(getQuote);
+});
+
+
+//http://codepen.io/marcywilliamspudalov/pen/PPqOmN
