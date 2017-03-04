376c362fb3dca71bb33bfd08b7038847ef5495d7
Mitul Shah <mitulxshah@gmail.com>
2017-03-04 14:10:33 -0500

    changing colours, non-repeating, shorter quotes, etc project complete 🔥

--- projects/randomQuotes/script.js
+++ projects/randomQuotes/script.js
@@ -1,35 +1,9 @@
-/*
-var authorName 	= document.querySelector('.author');
-var newQuote 	= document.querySelector('.quote');
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://quotes.stormconsultancy.co.uk/random.json", false);
-xhr.send();
-
-var quoteData = JSON.parse(xhr.response);
-
-var request = new XMLHttpRequest();
-var url = "http://quotes.stormconsultancy.co.uk/random.json";
-request.open("GET", url, true);
-
-function insert(){
-	location.reload();
-}
-
-request.onload = function(){
-	var data = JSON.parse(request.response);
-	authorName.innerHTML = data.author;
-	newQuote.innerHTML = data.quote;	
-}
-
-request.send();
-*/
-
 $(document).ready(getQuote);
 
-var colors = ['#e74c3c', '#3498db', '#2ecc71', '#9b59b6', '#f1c40f', '#34495e', '#4DAF7C', 'coral', '#F89406', '#22A7F0'];
+var colors = ['#e74c3c', '#3498db', '#2ecc71', '#9b59b6', 'darksalmon', '#34495e', '#4DAF7C', 'coral', '#F89406', '#22A7F0'];
 var quoteBox = document.querySelector('.quoteBox');
 var tweetBtn = document.querySelector('.tweet-btn');
+let lastColor;
 
 
 function getQuote() {
@@ -38,25 +12,35 @@ function getQuote() {
     jsonpCallback: "parseQuote",
     url: "http://quotes.stormconsultancy.co.uk/random.json",
     success: function(results) {
+	setTimeout(function(){
 		if(results.quote.length <= 150){
 			$('.quote').text(results.quote);	
 			$('.author').text(results.author);
 			$('.tweet-link').attr("href", 'https://twitter.com/intent/tweet?text=' + results.quote + ' - ' + results.author);
 		} else {
 			getQuote();	
-		}	
+		}
+	}, 330)	
     }
   });
 }
 
-function colorChange(){
+function quoteTransition(){
 	var random = colors[Math.floor(Math.random() * colors.length)];
-	quoteBox.style.backgroundColor = random;
+	if(random === lastColor){ // diff colour each time
+		return quoteTransition();
+	}
+		quoteBox.style.backgroundColor = random;
+		$('.quote').fadeOut(400);
+		$('.author').fadeOut(400);
+		$('.quote').fadeIn(1500);
+		$('.author').fadeIn(1500);
+		lastColor = random;
 }
 
 $(document).ready(function() {
   // get a random quote when page loads
   getQuote();
-  	$('.quote-btn').click(getQuote);
-	$('.quote-btn').click(colorChange);
+  	$('.quote-btn').click(quoteTransition);
+	$('.quote-btn').click(getQuote);
 });
