e044e6e2a9e05977cdfd46740adab6f18c05f69d
Mitul Shah <mitulxshah@gmail.com>
2017-03-08 00:11:16 -0500

    finished new algo

--- algorithms/script.js
+++ algorithms/script.js
@@ -50,3 +50,24 @@ function findLong(str) {
 
 findLong("The quick brown fox jumped over the lazy dog");
 // ==================== //
+
+
+
+// ==================== //
+// Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+  // You can do this!
+  var newArr = [0,0,0,0];
+  for(var i = 0; i < arr.length; i++){
+   for(var x = 0; x < arr[i].length; x++){
+     if(arr[i][x] > newArr[i]) {         
+          newArr[i] = arr[i][x];
+	 }
+   	}
+  }
+  return newArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// ==================== //
