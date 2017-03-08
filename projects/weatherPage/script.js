11dd4463f85817a99ba6dfec573766b7b5a8821e
Mitul Shah <mitulxshah@gmail.com>
2017-03-07 22:45:31 -0500

    api fix, new functions

--- projects/weatherPage/script.js
+++ projects/weatherPage/script.js
@@ -1,15 +1,17 @@
-var mainSkies = document.querySelector('#weatherText');
+$(document).ready(getWeather);
 
-//var quoteData = JSON.parse(xhr.response);
-var request = new XMLHttpRequest();
-var url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=4ddb1b82dfbc99cb9eedfc8c2f096deb";
-request.open("GET", url, true);
-
-request.onload = function(){
-	var data = JSON.parse(request.response);
-	mainSkies.innerHTML = data.visibility;
-}
-request.send();
+//var mainSkies = document.querySelector('#weatherText');
+//
+////var quoteData = JSON.parse(xhr.response);
+//var request = new XMLHttpRequest();
+//var url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=4ddb1b82dfbc99cb9eedfc8c2f096deb";
+//request.open("GET", url, true);
+//
+//request.onload = function(){
+//	var data = JSON.parse(request.response);
+//	mainSkies.innerHTML = data.visibility;
+//}
+//request.send();
 
 //
 //var getIP = 'http://ip-api.com/json/';
@@ -26,3 +28,39 @@ request.send();
 //})
 //
 //
+var API = "&APPID=4ddb1b82dfbc99cb9eedfc8c2f096deb";
+var mainUrl = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=4ddb1b82dfbc99cb9eedfc8c2f096deb";
+var url = "http://api.openweathermap.org/data/2.5/weather?q=London" + API + "&units=imperial";
+var degree = " ºF";
+
+function getWeather() {
+  $.ajax({
+    dataType: "json",
+    url: url,
+    success: function(results) {	
+  		updateData(results);
+	}
+  });
+ }
+
+function updateData(results){
+	
+	$('#weatherText').text(Math.round(results.main.temp) + degree);
+}
+
+function metricChange(){
+	if(degree === " ºF"){
+		url = mainUrl + "&units=metric";
+		degree = " ºC";
+		getWeather();
+	} else {
+		url = mainUrl + "&units=imperial";
+		degree = " ºF"
+		getWeather();
+	}	
+}
+
+$(document).ready(function(){
+	getWeather();
+	$('#metric').click(metricChange);
+});
