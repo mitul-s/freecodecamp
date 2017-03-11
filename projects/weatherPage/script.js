73c0a155f0a1a1765fff8926ef472cc41a1cb708
Mitul Shah <mitulxshah@gmail.com>
2017-03-11 18:44:41 -0500

    project compelte🔥  metric user story, added more fun stuff, cleaned up

--- projects/weatherPage/script.js
+++ projects/weatherPage/script.js
@@ -1,21 +1,20 @@
 $(document).ready(getWeather);
 var API = "&APPID=4ddb1b82dfbc99cb9eedfc8c2f096deb";
-var met = "&units=metric";
-var imp = "&units=imperial";
+var metric = document.querySelector('#metric');
 
 function getWeather() {
 	$.getJSON('http://ipinfo.io/json')
-		.then(function(results){
+		.then(function (results) {
 			var loc = results.loc.split(',');
-			var lon = loc[1].toString();
-			var lat = loc[0].toString();
-			console.log(lat, lon);
-			var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +  API + met;
+			var lon = loc[1].toString(), 
+				lat = loc[0].toString(), 
+				url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +  API + "&units=metric";
+			$('#location').text(results.city);
 		$.getJSON(url)
-			.then(function(results){
+			.then(function (results) {
 				updateData(results);
 			})
-			.fail(function(){
+			.fail(function() {
 				alert('error');
 		});
 	})
@@ -25,26 +24,28 @@ function getWeather() {
 }
 
 function updateData(results) {
-	$('#location').text(results.name);
 	$('#weatherText').text(Math.round(results.main.temp));
+	$('#humidity').text(results.main.humidity);
+	$('#wind').text(results.wind.speed);
+	$('#desc').text(results.weather[0].description);
 	var link = "https://source.unsplash.com/1600x1080/?"+ results.weather[0].main;
 	$('body').css("background-image", 'url('+ link +')'); 
 }
 
-function metricChange(){
-	alert('testing');
-//	if(degree === " ºC"){
-//		url = mainUrl + "&units=imperial";
-//		degree = " ºF";
-//		getWeather();
-//	} else {
-//		url = mainUrl + "&units=metric";
-//		degree = " ºC"
-//		getWeather();
-//	}	
+function metricChange(results){
+	var x;
+	if(metric.innerHTML === "ºC"){
+		x = document.querySelector('#weatherText').innerHTML * 9 / 5 + 32;
+		document.querySelector('#weatherText').innerHTML = Math.round(x);
+		metric.innerHTML = "ºF";
+	} else {
+		x = (document.querySelector('#weatherText').innerHTML - 32) * 5 / 9;
+		document.getElementById('weatherText').innerHTML = Math.round(x);
+		metric.innerHTML = "ºC";
+	}
 }
 
+
 $(document).ready(function(){
-	getWeather();
 	$('#metric').click(metricChange);
 });
