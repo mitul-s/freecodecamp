1d2d668f6bfa9a6c2f03727f497948ab127f1056
Mitul Shah <mitulxshah@gmail.com>
2017-03-15 23:02:55 -0400

    added desc, new functions, pretty much done

--- projects/wikiView/script.js
+++ projects/wikiView/script.js
@@ -1,8 +1,8 @@
 function dataLoad(){
 	var search = $('#search').val();
 	var $wiki = document.querySelector('.wiki');
-	
-//	$wiki.text("");
+
+	//$wiki.text("");
 	
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function(){
@@ -13,21 +13,17 @@ function dataLoad(){
         url: wikiUrl,
         dataType: "jsonp",
         jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-//			var desc = response[2];
-//			var link = response[3];
-			
+        success: function( response ) {            
+			var articleList = response[1];
+			var desc = response[2];
+				
 			if(search.length !== 0){
-				$('#lucky-btn').hide();
+				swipe();
             	for (var i = 0; i < articleList.length; i++) {
                 	articleStr = articleList[i];
-//					desc = desc[i];
                 	var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-					var newHTML = "<li class='wiki-list'><a href='" + url + "'>" + articleStr + "</a></li>";
+					var newHTML = "<a href='" + url + "'><li class='wiki-list'><h1>" + articleStr + "</h1><p id='description'>" + desc[i] + "</p></li></a>";
 					$(newHTML).appendTo($wiki);
-//					var newDesc = "<p>" + desc + "</p>";
-//					$(newDesc).appendTo($('.wiki-list')).fadeIn(1000);
             	};
 			} else {
 				alert("Please type something or make a random search!");
@@ -38,4 +34,16 @@ function dataLoad(){
     return false;
 };
 
+function swipe(){
+	$('.wrap').hide();
+	$('#title').hide();
+	$('.form-container').animate({
+		marginTop: '-70px',
+	}, 500);
+	var newBtn = '<button id="submit-btn" class="ui inverted button">Search</button>';
+	$(newBtn).appendTo('#form-container');	
+}
+
 $('#form-container').submit(dataLoad);
+$('#submit-btn').click(dataLoad);
+
