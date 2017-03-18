589f0f9a3c9680387f991c0309faf25867d4d24c
Mitul Shah <mitulxshah@gmail.com>
2017-03-18 14:05:13 -0400

    bug fixes

--- projects/wikiView/script.js
+++ projects/wikiView/script.js
@@ -1,27 +1,30 @@
+var holder = 0;
+var newBtn = '<button id="submit-btn" class="ui inverted button">Search</button>';
+
+
 function dataLoad(){
 	var search = $('#search').val();
 	var $wiki = document.querySelector('.wiki');
-
 	//$wiki.text("");
 	
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + search + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function(){
         $wiki.text("failed to get wikipedia resources");
     }, 8000);
-
+	
     $.ajax({
         url: wikiUrl,
         dataType: "jsonp",
         jsonp: "callback",
-        success: function( response ) {            
+        success: function( response ) { 
 			var articleList = response[1];
 			var desc = response[2];
-				
+
 			if(search.length !== 0){
 				swipe();
             	for (var i = 0; i < articleList.length; i++) {
-                	articleStr = articleList[i];
-                	var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+					articleStr = articleList[i];
+					var url = 'http://en.wikipedia.org/wiki/' + articleStr;
 					var newHTML = "<a href='" + url + "'><li class='wiki-list'><h1>" + articleStr + "</h1><p id='description'>" + desc[i] + "</p></li></a>";
 					$(newHTML).appendTo($wiki);
             	};
@@ -35,13 +38,15 @@ function dataLoad(){
 };
 
 function swipe(){
+	$('.form-container').animate({
+		marginTop: '-80px',
+	}, 600);
 	$('.wrap').hide();
 	$('#title').hide();
-	$('.form-container').animate({
-		marginTop: '-70px',
-	}, 500);
-	var newBtn = '<button id="submit-btn" class="ui inverted button">Search</button>';
-	$(newBtn).appendTo('#form-container');	
+	if(holder === 0){
+		$(newBtn).appendTo('#form-container');
+		holder = 1;
+	}
 }
 
 $('#form-container').submit(dataLoad);
